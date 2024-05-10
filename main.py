986bc0f7aeaecc17009caafa4159d4fd1c4aea49
nostalgiccat <nesdom27@gmail.com>
2024-05-10 15:08:58 -0700

    completed UI

--- main.py
+++ main.py
@@ -24,9 +24,19 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1,row=1)
 
+timer_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 32, "bold"), text="Timer")
+timer_label.grid(column=1, row=0)
 
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12))
+checkmark_label.grid(column=1, row=3)
 
 window.mainloop()
 

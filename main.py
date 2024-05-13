dfc55c0f1782b3b606b25a23dda54eec7b0b970e
nostalgiccat <nesdom27@gmail.com>
2024-05-13 15:30:00 -0700

    added functionality for the reset button

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = .1
-SHORT_BREAK_MIN = .1
-LONG_BREAK_MIN = .1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 reps = 0
-
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    #timer_text 00:00
+    canvas.itemconfig(timer_text, text=f"00:00")
+    #title_label "Timer"
+    title_label.config(text="Timer", fg=GREEN)
+    #reset check_marks
+    checkmark_label.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -37,6 +47,8 @@ def start_timer():
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
+    global timer
+    global marks
     count_min = math.floor(count / 60)
     count_sec = math.floor(count % 60)
     if count_sec < 10:
@@ -46,7 +58,7 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count-1)
+        timer = window.after(1000, count_down, count-1)
     else:
         start_timer()
         marks = ""
@@ -74,7 +86,7 @@ title_label.grid(column=1, row=0)
 start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", command=reset_timer, highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
 checkmark_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12))

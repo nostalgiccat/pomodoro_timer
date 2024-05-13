cc20a003d6614990236d2a2be76347bc4d81ae11
nostalgiccat <nesdom27@gmail.com>
2024-05-13 13:56:27 -0700

    setting different timer sessions and values

--- main.py
+++ main.py
@@ -9,12 +9,32 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(1.2*60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        title_label.config(text="Break", fg=RED)
+        title_label.grid(column=1, row=0)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        title_label.config(text="Break", fg=PINK)
+        title_label.grid(column=1, row=0)
+        count_down(short_break_sec)
+    else:
+        title_label.config(text="Work", fg=GREEN)
+        title_label.grid(column=1, row=0)
+        count_down(work_sec)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     count_min = math.floor(count / 60)
@@ -27,6 +47,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count-1)
+    else:
+        start_timer()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
@@ -39,8 +61,8 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1,row=1)
 
-timer_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 32, "bold"), text="Timer")
-timer_label.grid(column=1, row=0)
+title_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 32, "bold"), text="Timer")
+title_label.grid(column=1, row=0)
 
 start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
